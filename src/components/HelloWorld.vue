b5e41d8f5381f3524b50cbee98ffd1f42ea7b894
meme2wsx <meme2wsx@gmail.com>
2022-09-07 15:45:24 +0900

    tsxに変更

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
-<script lang="ts">
+<script lang="tsx">
+  import { VNode } from 'vue'
   import { Vue } from 'vue-class-component'
-
+  
   export default class HelloWorld extends Vue {
     msg = "Vueで羊をかぞえる"
     val = 1
@@ -8,17 +9,17 @@
     doAction() : void {
       this.val += 1
     }
-  }
-</script>
 
-<template>
-  <h1 class="bg-info text-white p-2">{{ msg }}</h1>
-  <div class="container">
-    <h2 class="my-2">枠内をクリックしてください</h2>
-    <div class="alert alert-info">
-      <h3 v-on:click="doAction">羊が {{ val }} 匹</h3> 
-    </div>
-    <h5>画面リロードでリセットします</h5>
-  </div>
-  
-</template>
+    render():VNode{
+      return(<div>
+        <h1 class="bg-info text-white p-2">{this.msg}</h1>
+        <div class="container">
+          <h2 class="my-2">枠内をクリックしてください</h2>
+          <div class="alert alert-info">
+            <h3 onclick={this.doAction}>羊が {this.val} 匹</h3>
+          </div>
+        </div>
+      </div>)
+  }
+}
+</script>
